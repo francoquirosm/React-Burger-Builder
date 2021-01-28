7dbac39d00aa5635a75c25ab3e1b1fa9f6e23fd3
Franco <fquiros@akurey.com>
2021-01-27 20:07:51 -0600

    added layout and burger builder page:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Layout from "./components/Layout";
+import BurgerBuilder from "./containers/BurguerBuilder";
 class App extends Component {
   render() {
     return (
-      <div >
-        
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
